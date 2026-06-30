32b08e7538586f3ac6a2dd55118f006f59d62d4e
AtaberkBY <yilmazataberkberke@gmail.com>
2026-06-30 20:02:24 +0300

    feat(dev): establish DB connection and models synchronization

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 require('dotenv').config();
+
 const express = require('express');
+const sequelize = require('./config/database');
+
+const User = require('./models/User');
+const Product = require('./models/Product');
+const Cart = require('./models/Cart');
+const CartItem = require('./models/CartItem');
+const Order = require('./models/Order');
+const OrderItem = require('./models/OrderItem');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`test, port: ${PORT}`);
-})
 
+const SyncDatabase = async () => {
+    try {
+        await sequelize.authenticate();;
+        console.log('Database connection has been established successfully.');
+
+        await sequelize.sync({ alter: true });
+        console.log('Database synced successfully.');
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        });
+
+    }
+    catch  (err) {
+        console.error('Error syncing database:', err);
+    }
+}
+
+
+
+
+SyncDatabase();
